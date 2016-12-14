d4d72621d17d1e5f68e2a861297f91d07aa1936d
Tom Panning <tom.panning@nextcentury.com>
2016-12-14 18:02:08 -0500

    Use alternatives configuration for JAVA_HOME when available

--- source/50_java.sh
+++ source/50_java.sh
@@ -1,1 +1,6 @@
-export JAVA_HOME="/usr/java/latest"
+# Use alternatives configuration if it's available, otherwise assume /usr/java/latest is correct
+if [ -a /etc/alternatives/java_sdk ] ; then
+    export JAVA_HOME="/etc/alternatives/java_sdk"
+else
+    export JAVA_HOME="/usr/java/latest"
+fi
